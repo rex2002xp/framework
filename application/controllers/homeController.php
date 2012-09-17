1811f540bff2b334216a9f08ca5c3118569a9752
Victor Cornejo <victor.cornejo@iguanadas.com>
2012-09-16 23:56:45 -0600

    Se incorpora al Framework el motor de plantillas Twig.

--- application/controllers/homeController.php
+++ application/controllers/homeController.php
@@ -10,11 +10,19 @@ namespace application\controllers;
 class homeController extends \core\Controller{
     
     public function __construct() {
-        parent::__construct();        
+        parent::__construct();
+        
     }
 
     public function index() {
-        print 'Hola Mundo';
+        include_once ROOT . 'lib' . DS . 'Twig' . DS . 'Autoloader.php';
+        \Twig_Autoloader::register();
+        $path = ROOT . 'templates' . DS . 'default'.DS;
+        $loader = new \Twig_Loader_Filesystem($path);
+        $twig = new \Twig_Environment($loader, array('cache' => ROOT . 'cache','debug'=>true,));
+        
+        $template = $twig->loadTemplate('index.html.twig');        
+        echo $twig->render('index.html.twig', array('name' => 'Fabien'));
     }
 }
 
--- web/index.php
+++ web/index.php
@@ -3,8 +3,8 @@
  * Punto de entrada de la aplicacion. * 
  * @author Victor Hugo Cornejo Calderon <victor.cornejo@iguanadas.com>
  * @filesource index.php
- */   
-$inicio = microtime();
+ */
+
 
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', realpath(dirname(__DIR__)) . DS);
@@ -22,10 +22,4 @@ catch (Exception $ex)
 {
     print_r($ex->getMessage());
 }
-print_r('</br>');
-print_r("Segundos: ");
-print_r(microtime()-$inicio);
-print_r('</br>');
-print_r('Usados:' . memory_get_usage()/1024 . ' Kbyte');
-
 ?>
